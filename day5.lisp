d2db4b21e0baec5cc3d311ef32ce55062ed07179
gingeralesy <gingeralesy@gmail.com>
2024-12-05 17:20:44 +0200

    Improvements

--- day5.lisp
+++ day5.lisp
@@ -2,24 +2,19 @@
 
 ;; https://adventofcode.com/2024/day/5
 
-;; TODO: Make a faster to check map out of the order rules rather than having to search them.
-
 (deftype d5-page () '(integer 0 99))
 
-(defun day5-data (&optional sort-p)
+(defun day5-data ()
   (declare (optimize (speed 3)))
   (let ((page-parser (cl-ppcre:create-scanner "^(\\d+)\\|(\\d+)$"))
-        (page-orders (make-queue))
-        (page-count 0)
+        (page-orders (make-array 100 :element-type 'list :initial-element ()))
         (update-parser (cl-ppcre:create-scanner "\\b\\d+\\b"))
         (updates (make-queue))
         (update-count 0)
         (page-orders-p T))
-    (declare (type (unsigned-byte 32) page-count update-count))
+    (declare (type (unsigned-byte 32) update-count))
     (do-file (line "day5.txt" (values
-                               (make-array page-count
-                                           :element-type '(simple-array d5-page (2))
-                                           :initial-contents (queue-as-list page-orders))
+                               page-orders
                                (make-array update-count
                                            :element-type '(simple-array d5-page (*))
                                            :initial-contents (queue-as-list updates))))
@@ -29,19 +24,10 @@
          (multiple-value-bind (match groups) (cl-ppcre:scan-to-strings page-parser line)
            (unless match (error "Invalid page order line: ~a" line))
            (let* ((a (parse-integer (svref groups 0)))
-                  (b (parse-integer (svref groups 1)))
-                  (page-pair (make-array 2 :element-type 'd5-page
-                                           :initial-contents (list a b))))
+                  (b (parse-integer (svref groups 1))))
+             (declare (type d5-page a b))
              (when (= a b) (error "Cannot order the same page with itself."))
-             (if sort-p
-                 (pqueue-push page-pair page-orders #'identity
-                              #'(lambda (a b)
-                                  (declare (type (simple-array d5-page (2)) a b))
-                                  (or (< (aref a 0) (aref b 0))
-                                      (and (= (aref a 0) (aref b 0))
-                                           (< (aref a 1) (aref b 1))))))
-                 (queue-push page-pair page-orders))
-             (incf page-count))))
+             (setf (aref page-orders a) (sort (push b (aref page-orders a)) #'<)))))
         (T
          (loop for page in (cl-ppcre:all-matches-as-strings update-parser line)
                count page into size
@@ -52,31 +38,26 @@
          (incf update-count))))))
 
 (defun day5-validate (orders update)
-  (declare (type (simple-array (simple-array d5-page (2)) (*)) orders))
+  (declare (type (simple-array list (100)) orders))
   (declare (type (simple-array d5-page (*)) update))
   (declare (optimize (speed 3)))
   (loop with valid-p = T
         while valid-p
-        for page-a of-type d5-page across update
-        count page-a into current
+        for index-a of-type (unsigned-byte 32) from 1 below (length update)
+        for page-a of-type d5-page = (aref update index-a)
+        for before-a of-type list = (aref orders page-a)
         do (loop while valid-p
-                 for order across orders
-                 for order-a of-type d5-page = (aref order 0)
-                 for order-b of-type d5-page = (aref order 1)
-                 until (< page-a order-a)
-                 when (= page-a order-a)
-                 do (loop while valid-p
-                          for page-b of-type d5-page across update
-                          count page-b into other
-                          until (< current other)
-                          when (= page-b order-b)
-                          do (setf valid-p NIL)))
+                 for index-b of-type (unsigned-byte 32) from 0 below index-a
+                 for page-b of-type d5-page = (aref update index-b)
+                 when (find page-b before-a :test #'(lambda (a b)
+                                                      (declare (type d5-page a b))
+                                                      (= a b)))
+                 do (setf valid-p NIL))
         finally (return valid-p)))
 
 (defun d5p1 ()
   (declare (optimize (speed 3)))
-  (multiple-value-bind (page-orders updates)
-      (day5-data T)
+  (multiple-value-bind (page-orders updates) (day5-data)
     (declare (type (simple-array (simple-array d5-page (*)) (*)) updates))
     (loop for update of-type (simple-array d5-page (*)) across updates
           when (day5-validate page-orders update)
@@ -87,39 +68,34 @@
 ;; 6951
 
 (defun day5-fix (orders update)
-  (declare (type (simple-array (simple-array d5-page (2)) (*)) orders))
+  (declare (type (simple-array list (100)) orders))
   (declare (type (simple-array d5-page (*)) update))
   (declare (optimize (speed 3)))
   (loop
+    with done-to of-type (unsigned-byte 32) = 0
     do (loop
-         ;; If there's a swap, start from the beginning.
-         ;; TODO: Optimise so that it doesn't check parts that are already sorted.
          with swap-p = NIL
          until swap-p
-         for index-a of-type (unsigned-byte 32) from 1 below (length update)
+         for index-a of-type (unsigned-byte 32) from (1+ done-to) below (length update)
          for page-a of-type d5-page = (aref update index-a)
+         for before-a of-type list = (aref orders page-a)
          do (loop
               until swap-p
-              for order across orders
-              for order-a of-type d5-page = (aref order 0)
-              for order-b of-type d5-page = (aref order 1)
-              until (< page-a order-a)
-              when (= page-a order-a)
-              do (loop
-                   until swap-p
-                   for index-b of-type (unsigned-byte 32) from 0 below index-a
-                   for page-b of-type d5-page = (aref update index-b)
-                   do (when (= page-b order-b)
-                        (setf (aref update index-a) page-b)
-                        (setf (aref update index-b) page-a)
-                        (setf swap-p T)))))
+              for index-b of-type (unsigned-byte 32) from 0 below index-a
+              for page-b of-type d5-page = (aref update index-b)
+              do (when (find page-b before-a :test #'(lambda (a b)
+                                                       (declare (type d5-page a b))
+                                                       (= a b)))
+                   (setf (aref update index-a) page-b)
+                   (setf (aref update index-b) page-a)
+                   (setf swap-p T)))
+         unless swap-p do (setf done-to index-a))
     until (day5-validate orders update)
     finally (return update)))
 
 (defun d5p2 ()
   (declare (optimize (speed 3)))
-  (multiple-value-bind (page-orders updates)
-      (day5-data T)
+  (multiple-value-bind (page-orders updates) (day5-data)
     (declare (type (simple-array (simple-array d5-page (*)) (*)) updates))
     (loop for update of-type (simple-array d5-page (*)) across updates
           unless (day5-validate page-orders update)
