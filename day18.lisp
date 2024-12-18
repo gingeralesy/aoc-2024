992a4e67b7be56522a307c3c01cbd8025fd76098
gingeralesy <gingeralesy@gmail.com>
2024-12-18 11:13:19 +0200

    Don't check unnecessarily

--- day18.lisp
+++ day18.lisp
@@ -27,7 +27,11 @@
                                                     :initial-contents (queue-as-list coordinates))
                                   count)))))
 
-(defun day18-solve (start-x start-y end-x end-y map width height)
+(defun day18-solve (start-x start-y end-x end-y map width height &optional out-path)
+  (declare (type (unsigned-byte 32) start-x start-y end-x end-y width height))
+  (declare (type (simple-array boolean (* *)) map))
+  (declare (type (or null (simple-array boolean (* *))) map out-path))
+  (declare (optimize (speed 3)))
   (let* ((f-score (make-array `(,height ,width) :element-type '(unsigned-byte 32)
                                                 :initial-element #xffffffff))
          (g-score (make-array `(,height ,width) :element-type '(unsigned-byte 32)
@@ -42,18 +46,21 @@
     (flet ((manhattan (from-x from-y &optional (to-x end-x) (to-y end-y))
              (+ (abs (- to-x from-x)) (abs (- to-y from-y))))
            (neighbours (x y)
+             (declare (type (unsigned-byte 32) x y))
              (loop for dx in '(-1  1  0  0)
                    and dy in '( 0  0 -1  1)
-                   for x1 = (+ x dx)
-                   and y1 = (+ y dy)
+                   for x1 of-type (signed-byte 32) = (+ x dx)
+                   and y1 of-type (signed-byte 32) = (+ y dy)
                    when (and (<= 0 x1) (<= 0 y1) (< x1 width) (< y1 height) (not (aref map y1 x1)))
                    collect (cons x1 y1)))
            (reconstruct-path ()
              (loop for x = end-x then (car prev)
                    and y = end-y then (cdr prev)
                    for prev = (aref came-from y x)
-                   ;; collect (cons x y) into path
-                   while prev count prev)))
+                   collect (cons x y) into path
+                   when out-path do (setf (aref out-path y x) T)
+                   while prev count prev into count
+                   finally (return (values count path)))))
       (setf (aref f-score start-y start-x) 0)
       (setf (aref g-score start-y start-x) (manhattan start-x start-y))
       (heap-push open-set (cons start-x start-y))
@@ -77,17 +84,25 @@
 
 (defun d18p1 (&optional test-p)
   (multiple-value-bind (map width height) (day18-data (if test-p 12 1024) test-p)
-    (day18-solve 0 0 (if test-p 6 70) (if test-p 6 70) map width height)))
+    (nth-value 0 (day18-solve 0 0 (if test-p 6 70) (if test-p 6 70) map width height))))
 
 ;; 318
 
 (defun d18p2 (&optional test-p)
   (let ((limit (if test-p 12 1024)))
     (multiple-value-bind (map width height remaining count) (day18-data limit test-p)
-      (loop for i from 0 below count
+      (loop with path = (make-array `(,height ,width) :element-type 'boolean :initial-element NIL)
+            and first-p = T
+            and stuck-p = NIL
+            and end-x = (if test-p 6 70)
+            and end-y = (if test-p 6 70)
+            for i from 0 below count
             for byte = (aref remaining i)
             do (setf (aref map (cdr byte) (car byte)) T)
-            while (day18-solve 0 0 (if test-p 6 70) (if test-p 6 70) map width height)
+            when (or first-p (aref path (cdr byte) (car byte)))
+            do (setf stuck-p (null (day18-solve 0 0 end-x end-y map width height path)))
+            do (setf first-p NIL)
+            until stuck-p
             finally (return (values byte (+ limit (1+ i))))))))
 
 ;; 56,29
